ee17598b29d7f612fa1257fe608430f37c449c76
sunshine1027 <sunshine10271993@gmail.com>
2017-01-04 00:19:37 +0800

    remove old module

--- abstract-factory/src/main/java/com/sunshine1027/abstractfactory/games/pc/WOWGame.java
+++ abstract-factory/src/main/java/com/sunshine1027/abstractfactory/games/pc/WOWGame.java
@@ -8,11 +8,11 @@ import com.sunshine1027.abstractfactory.User;
 
 public class WOWGame implements PCGame {
 
-    public String storageProgress(User user, int times) {
+    public String storageProgress(final User user, final int times) {
         return createMsg(user, times);
     }
 
-    public static String createMsg(User user, int times) {
+    public static String createMsg(final User user, final int times) {
         return "This is the " + times + " times User " + user.getUsername() + " storage progress in WOW";
     }
 }
